0b53dad671a4d59cd15becafb541dce1124a559b
Marcel Thomas <marcelthomas5@gmail.com>
2022-06-30 17:00:44 +0100

    Testing counter

--- nubo-website/components/templates/basic/Basic.tsx
+++ nubo-website/components/templates/basic/Basic.tsx
@@ -2,6 +2,7 @@
 import { h, FunctionalComponent } from 'preact';
 import { tw } from '@twind';
 import { Page } from '@nubo-shared/pages/mod.ts';
+import Counter from '../../../islands/Counter.tsx';
 
 interface Props {
   page: Page;
@@ -12,6 +13,8 @@ export const BasicTemplate: FunctionalComponent<Props> = ({ page }) => {
     <div class={tw`flex h-screen`}>
       <div class={tw`m-auto`}>
         <h1 class={tw`text-6xl text-center`}>{page.title}</h1>
+
+        <Counter start={5} />
       </div>
     </div>
   );
