caccff479b8e99bad72b30c9491cfbb817deafd5
Marcel Thomas <marcelthomas5@gmail.com>
2022-07-01 10:45:05 +0100

    Update config.ts

--- nubo-api/config.ts
+++ nubo-api/config.ts
@@ -1,13 +1,11 @@
 import 'https://deno.land/x/dotenv/load.ts';
-import { logger } from './deps.ts';
 
 const name = 'nubo-api';
 const version = '0.0.1';
 const secretKey = Deno.env.get('NUBO_SECRET_KEY') || '';
 
 if (!secretKey) {
-  logger.error('Missing environment variable: NUBO_SECRET_KEY');
-  Deno.exit(1);
+  throw new Error('Missing environment variable: NUBO_SECRET_KEY');
 }
 
 export const config = {
