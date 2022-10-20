e36fb1775522c632a77676862a4d4bd21fcfe407
Marcel Thomas <marcelthomas5@gmail.com>
2022-10-20 09:47:12 +0100

    Added website test

--- apps/website/src/components/Test.tsx
+++ apps/website/src/components/Test.tsx
@@ -1,13 +1,13 @@
-// import type { Page } from '@nubojs/data';
+import type { Page, Website } from '@nubojs/data';
 import { Button } from '@nubojs/ui';
 import { FC, useEffect } from 'react';
 
 interface Props {
-  // pages: Page[];
-  pages: any[];
+  websites: Website[];
+  pages: Page[];
 }
 
-const Test: FC<Props> = ({ pages }) => {
+const Test: FC<Props> = ({ websites, pages }) => {
   useEffect(() => {
     const fn = async () => {
       // console.log(client);
@@ -22,9 +22,19 @@ const Test: FC<Props> = ({ pages }) => {
   }, []);
   return (
     <div>
-      {pages.map((page) => (
-        <div key={page.id}>{page.title}</div>
-      ))}
+      <div className="py-10">
+        <h2 className="text-lg font-bold">Websites</h2>
+        {websites.map((website) => (
+          <div key={website.id}>{website.name}</div>
+        ))}
+      </div>
+
+      <div className="py-10">
+        <h2 className="text-lg font-bold">Pages</h2>
+        {pages.map((page) => (
+          <div key={page.id}>{page.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
